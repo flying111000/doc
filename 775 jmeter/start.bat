c09112ef005f1986bfe302e0c008a8f2aa496d8f
Arthur <2853759768@qq.com>
2017-12-15 17:31:38 -0800

    php etc install

---
+++ 775 jmeter/start.bat
@@ -0,0 +1,1 @@
+jmeter -n -t C:/work/software_install/apache-jmeter-3.2/bin/templates/build-web-test-plan.jmx
---
+++ 993 code base/002 php/startPhpNginx.bat
@@ -0,0 +1,2 @@
+start C:/phpStudy/php/php-7.0.12-nts/php-cgi.exe -b 127.0.0.1:9000 -c C:/phpStudy/php/php-7.0.12-nts/php.ini
+start C:/phpStudy/nginx/nginx.exe -p C:/phpStudy/nginx
